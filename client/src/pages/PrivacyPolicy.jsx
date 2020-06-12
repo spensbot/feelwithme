48334550d1d885ee9c8601e753f2949b996156e6
Spenser Saling <spenser0saling@gmail.com>
2020-06-12 13:32:41 -0700

    Add Privacy Policy and Terms and Conditions

---
+++ client/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import { Typography } from '@material-ui/core'
+
+export default function PrivacyPolicy() {
+  return (
+    <Layout>
+      <Typography>Privacy Policy</Typography>
+      <Typography>Blah, blah, blah.</Typography>
+    </Layout>
+  )
+}
---
+++ client/src/pages/Terms.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import { Typography } from '@material-ui/core'
+
+export default function Terms() {
+  return (
+    <Layout>
+      <Typography>Terms and Conditions</Typography>
+      <Typography>Blah, blah, blah.</Typography>
+    </Layout>
+  )
+}
