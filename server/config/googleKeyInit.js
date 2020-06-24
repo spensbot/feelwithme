0ec75bc88ca4bdf9037fa9760b049cfe9b694685
Spenser Saling <spenser0saling@gmail.com>
2020-06-24 10:29:18 -0700

    Fix google key creation bug

--- server/config/googleKeyInit.js
+++ server/config/googleKeyInit.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 //Create json file 
-googleKeyPath='./secret/googleKey.json'
+googleKeyPath='googleKey.json'
 fs.writeFile(googleKeyPath, process.env.GOOGLE_JSON_KEY, function (err) {
   if (err) throw err;
   console.log('Google Key File Created!');
